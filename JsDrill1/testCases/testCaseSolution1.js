ad4bc97ff467e2df7dd952dcc39f24a43807b224
Pritam Wankhede <109061110+PritamWankhede@users.noreply.github.com>
2024-09-05 00:24:36 +0530

    Update testCaseSolution1.js

--- JsDrill1/testCases/testCaseSolution1.js
+++ JsDrill1/testCases/testCaseSolution1.js
@@ -2,3 +2,5 @@ const {personsEmail} = require('../Solutions/Solution1.js');
 const {arrayOfObjects} = require('../Person.js');
 
 console.log(personsEmail(arrayOfObjects));
+console.log(personsEmail());
+console.log(personsEmail(String));
