bf1150fda6bca0e5a7a81070e1fce0bbc16ced63
Pritam Wankhede <109061110+PritamWankhede@users.noreply.github.com>
2024-09-05 10:43:16 +0530

    testCaseSolution7.js

--- JsDrill1/testCases/testCaseSolution7.js
+++ JsDrill1/testCases/testCaseSolution7.js
@@ -2,3 +2,6 @@ const {solution7} = require('../Solutions/Solution7.js');
 const {arrayOfObjects} = require('../Person.js');
 
 console.log(solution7(arrayOfObjects));
+console.log(solution7());
+console.log(solution7([]));
+
