878a98b0e8ad595f4f278d7358362145de79b6d0
Pritam Wankhede <109061110+PritamWankhede@users.noreply.github.com>
2024-09-05 10:44:07 +0530

    testCaseSolution8.js

--- JsDrill1/testCases/testCaseSolution8.js
+++ JsDrill1/testCases/testCaseSolution8.js
@@ -3,4 +3,7 @@
  const {session8} = require('../Solutions/Solution8.js');
  const {arrayOfObjects} = require('../Person.js');
 
- console.log(session8(arrayOfObjects));
+
+console.log(session8(arrayOfObjects));
+ console.log(session8());
+ console.log(session8([]));
